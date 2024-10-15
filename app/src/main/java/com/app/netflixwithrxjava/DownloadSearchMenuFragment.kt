5fcf786a925a73d9dfa30d0b48902989b04bab62
krunal21041993 <kathikar.krunal3@gmail.com>
2024-10-15 21:55:15 +0530

    Navigation Problem Resolved

--- app/src/main/java/com/app/netflixwithrxjava/DownloadSearchMenuFragment.kt
+++ app/src/main/java/com/app/netflixwithrxjava/DownloadSearchMenuFragment.kt
@@ -10,7 +10,9 @@ import androidx.annotation.CallSuper
 import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
+import androidx.navigation.NavDeepLink
 import androidx.navigation.NavDeepLinkRequest
+import androidx.navigation.NavOptions
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
@@ -33,10 +35,12 @@ abstract class DownloadSearchMenuFragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         android.R.id.home -> findNavController().navigateUp()
         R.id.fragmentDownloads -> {
-            findNavController().navigate(Uri.parse("example://secondary"))
+            findNavController().navigate(
+                Uri.parse("example://secondary"),
+                NavOptions.Builder().setRestoreState(true).build()
+            )
             super.onOptionsItemSelected(item)
         }
-
         else -> NavigationUI.onNavDestinationSelected(
             item,
             findNavController()
