52a8d54b47bfab6cbef5762361875d835e5fd406
krunal21041993 <kathikar.krunal3@gmail.com>
2025-01-26 15:44:31 +0530

    Refactoring Dependecies Completed

--- feature/ui/getstarted/build.gradle.kts
+++ feature/ui/getstarted/build.gradle.kts
@@ -39,15 +39,15 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.viewpagerindicator)
+    implementation(libs.expandablelayout)
 
-    implementation("it.xabaras.android:viewpagerindicator:2.0")
-    implementation("com.github.cachapa:ExpandableLayout:2.9.2")
+    testImplementation(libs.junit)
+
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 
     implementation(project(":android_resource"))
 }
--- feature/ui/splash/build.gradle.kts
+++ feature/ui/splash/build.gradle.kts
@@ -33,15 +33,15 @@ android {
 }
 
 dependencies {
-
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.lottie)
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("com.airbnb.android:lottie:6.5.2")
 }
